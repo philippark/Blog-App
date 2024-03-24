f6620fc331db272c88ed8e00844e23ee88268383
philippark <philippark271@gmail.com>
2024-03-23 21:23:05 -0400

    tutorial 15

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -7,15 +7,22 @@ const Home = () => {
         {title: "CHESS DRAMA!!!", body: "HANS NIEMANN!!!", author: "GothamChess", id: 2}
     ]);
 
+    const [name, setName] = useState("mario");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog)=>blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect( () => {
+        console.log("effect ran");
+    }, [name])
+
     return (  
         <div className = "Home">
             <BlogList blogs = {blogs} title = "All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs = {blogs.filter((blog)=>blog.author === "Daniel Naroditsky")} title = "Naroditsky Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName(name + "!")}>change name</button>
+            <p>{ name }</p>
         </div>
     );
 }
