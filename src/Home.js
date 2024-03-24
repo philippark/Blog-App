9d9cfd76b65a3ae19e61974b85aea70a689cbfe8
philippark <philippark271@gmail.com>
2024-03-24 19:26:05 -0400

    tutorial 18 added loading message

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(()=>{
         fetch("http://localhost:8000/blogs")
@@ -11,11 +12,13 @@ const Home = () => {
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
             })
     }, [])
 
     return (  
         <div className = "Home">
+            {isPending && <div>Loading...</div>}
             { blogs && <BlogList blogs = {blogs} title = "All Blogs"/> }
         </div>
     );
