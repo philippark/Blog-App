fb11b06e29a5ee03d22b8b7ebafd52ee3db4189f
philippark <philippark271@gmail.com>
2024-03-27 13:30:38 -0400

    tutorial 23: added react links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
+import {Link} from "react-router-dom"
+
 const Navbar = () => {
     return ( 
         <nav className = "navbar">
             <h1>DATABASE by ChessBase</h1>
             <div className = "links">
-                <a href="/" style = {{
+                <Link to="/" style = {{
                     color: "white",
                     backgroundColor: "black",
                     borderRadius: "8px"
-                }}>Home</a>
-                <a href="/create">New Blog</a>
+                }}>Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
