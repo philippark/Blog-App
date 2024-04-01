d7ab211c60e67f29584df3a726056a8292b7d81d
philippark <philippark271@gmail.com>
2024-03-31 20:45:21 -0400

    25: route parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from './Navbar.js';
 import Home from './Home.js'
 import Create from "./Create.js"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min.js';
+import BlogDetails from './BlogDetails.js';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
             </Route>
             <Route path = "/create">
               <Create />
+            </Route>
+            <Route path = "/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
