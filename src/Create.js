2d4b2f5937effe773ce4c5d701066f509857f9a2
philippark <philippark271@gmail.com>
2024-04-07 19:56:29 -0400

    blog create

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,25 @@ const Create = () => {
     return ( 
         <div className = "create">
             <h2>Create</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                />
+
+                <label>Blog content:</label>
+                <textarea required/>
+
+                <label>Blog author:</label>
+                <select>
+                    <option value="Gotham">Gotham</option>
+                    <option value="Danya">Danya</option>
+                    <option value="Botez">Botez</option>
+                </select>
+                <button>Add Blog</button>
+
+            </form>
         </div>
      );
 }
