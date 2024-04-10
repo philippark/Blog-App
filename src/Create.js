0e5b1d6cad90c54f8634b1bdfb7ddd6b23a9d812
philippark <philippark271@gmail.com>
2024-04-09 21:19:36 -0400

    added functionality for creating blogs

--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,35 @@
+import {useState} from 'react';
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Gotham");
+
     return ( 
         <div className = "create">
+
             <h2>Create</h2>
             <form>
                 <label>Blog title:</label>
                 <input 
                     type="text"
                     required
+                    value = {title}
+                    onChange={(e)=>setTitle(e.target.value)}
                 />
 
                 <label>Blog content:</label>
-                <textarea required/>
+                <textarea 
+                    required
+                    value = {body}
+                    onChange={(e)=>setBody(e.target.value)}
+                />
 
                 <label>Blog author:</label>
-                <select>
+                <select 
+                value = {author}
+                onChange={(e)=>setAuthor(e.target.value)}
+                >
                     <option value="Gotham">Gotham</option>
                     <option value="Danya">Danya</option>
                     <option value="Botez">Botez</option>
