28c1a252fc465851cefd34b8feb6d8877c2b1c6a
philippark <philippark271@gmail.com>
2024-04-12 20:22:01 -0400

    add blog fetches and updates json

--- src/Create.js
+++ src/Create.js
@@ -9,7 +9,11 @@ const Create = () => {
         e.preventDefault();
         const blog = {title, body, author};
 
-        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        })
     }
 
     return ( 
